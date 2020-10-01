c884331b25f43029064244a7ce62765c8bc10982
moin2672 <moin2672@gmail.com>
2020-10-01 09:55:28 +0530

    10. Using Node Express

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const http = require('http');
-
-const server = http.createServer((req, res) => {
-    res.end('This is my 1st response');
-})
+const app =  require('./backend/app');
 
 const port = process.env.PORT || 3000;
+
+app.set('port', port)
+const server = http.createServer(app)
+
+
 server.listen(port, () => console.log(`Listening on port ${port}..`))
