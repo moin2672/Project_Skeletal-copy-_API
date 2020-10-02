01ddc855e8032ee9d4e32586ecf9cb4243939fe3
moin2672 <moin2672@gmail.com>
2020-10-02 12:27:09 +0530

    15. adding Post method

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
+const bodyParser = require('body-parser')
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 //Adding middleware to resolve CORS problem
 app.use((req, res, next)=>{
@@ -9,6 +12,12 @@ app.use((req, res, next)=>{
     res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS');
     next()
 });
+
+app.post('/api/posts',(req, res, next) =>{
+    const post=req.body;
+    console.log(post);
+    res.status(201).json({message:"Post added successfully!"});
+})
 
 app.use('/api/posts',(req, res, next)=>{
     const posts=[{id:"asdf1adf",title:"title", content: "content"},{id:"asdf2adf",title:"title", content: "content"},{id:"asdf3adf", title:"title", content: "content"}];
