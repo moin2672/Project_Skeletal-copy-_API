d2e622278fd3cc1d0d9a8c9a0c638eb1a4f41441
moin2672 <moin2672@gmail.com>
2020-10-02 11:10:26 +0530

    12. Fetching initial Posts

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,14 @@ const express = require('express');
 const app = express();
 
 //middleware
-app.use((req, res, next)=>{
-    console.log('First Middleware')
-    next()
-});
+// app.use((req, res, next)=>{
+//     console.log('First Middleware')
+//     next()
+// });
 
-app.use((req, res, next)=>{
-    res.send('Hello From Express')
+app.use('/api/posts',(req, res, next)=>{
+    const posts=[{title:"title", content: "content"},{title:"title", content: "content"},{title:"title", content: "content"}];
+    res.status(200).json({message:"Post fetched successfully", posts:posts});
 });
 
 module.exports = app;
