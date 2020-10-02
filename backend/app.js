72dffd7400060a4b8d2ef4faf2d7b8fe1fe41320
moin2672 <moin2672@gmail.com>
2020-10-02 11:47:43 +0530

    14. Resolving CORS problem

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 
 const app = express();
 
-//middleware
-// app.use((req, res, next)=>{
-//     console.log('First Middleware')
-//     next()
-// });
+//Adding middleware to resolve CORS problem
+app.use((req, res, next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods','GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    next()
+});
 
 app.use('/api/posts',(req, res, next)=>{
-    const posts=[{title:"title", content: "content"},{title:"title", content: "content"},{title:"title", content: "content"}];
+    const posts=[{id:"asdf1adf",title:"title", content: "content"},{id:"asdf2adf",title:"title", content: "content"},{id:"asdf3adf", title:"title", content: "content"}];
     res.status(200).json({message:"Post fetched successfully", posts:posts});
 });
 
